1835443f95c9e5968b49f89454caa9ce68495ced
Sarah Doran <sarahdoran@wifi_stu-10-38-81-148.uncc.edu>
2017-01-17 20:41:00 -0500

    Have the firebase and table appending working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,82 @@
 // moment().format();
 
-var trainInfo = {
+$(document).ready(function() {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC--41JiUjnQnGRNfkEgPKK3N4niLiu3eo",
+    authDomain: "train-schedule-cd917.firebaseapp.com",
+    databaseURL: "https://train-schedule-cd917.firebaseio.com",
+    storageBucket: "train-schedule-cd917.appspot.com",
+    messagingSenderId: "493701823077"
+  };
+
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
+  var trainInfo = {
     trainName: "",
     destination: "",
     firstTrainTime: "",
     frequency: "",
-    arrival: "",
     nextArrival: "",
     minutesAway: "",
     addRow: function(){
-        console.log("Choo Choo!")
-        var newRow = $("#trainSchedule").children().eq(1).children("tr").eq(0).children("td");
-        newRow.eq(0).text($("#trainName").val().trim());
-        newRow.eq(1).text($("#destination").val().trim());
-        newRow.eq(2).text(parseInt($("#frequency").val().trim()));
-        // arrival.text(moment arrival calc.);
-        // minutesAway.text(moment minutes away calc.);
-   }
 
-}
+      trainInfo.trainName = $("#trainName").val().trim();
+      trainInfo.destination = $("#destination").val().trim();
+      trainInfo.firstTrainTime = $("#time").val().trim();
+      trainInfo.frequency = parseInt($("#frequency").val().trim());
 
-$(document).ready(function() {
- // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyC--41JiUjnQnGRNfkEgPKK3N4niLiu3eo",
-  //   authDomain: "train-schedule-cd917.firebaseapp.com",
-  //   databaseURL: "https://train-schedule-cd917.firebaseio.com",
-  //   storageBucket: "train-schedule-cd917.appspot.com",
-  //   messagingSenderId: "493701823077"
-  // };
-  // firebase.initializeApp(config);
+      database.ref().set({
+      trainName: trainInfo.trainName,
+      destination: trainInfo.destination,
+      frequency: trainInfo.frequency,
+      firstTrainTime: trainInfo.firstTrainTime,
+      // nextArrival: moment().format(trainInfo.firstTrainTime + trainInfo.frequency),
+      // minutesAway: moment().format(current time - (trainInfo.firstTrainTime + trainInfo.frequency))
+      });
+
+      database.ref().on("value", function(snapshot){
+        console.log(snapshot.val().trainName);
+        console.log(snapshot.val().destination);
+        console.log(snapshot.val().frequency);
+        console.log(snapshot.val().firstTrainTime);
+
+      var newRow = $("<tr class='newRow'>");
+
+      var newCell1 = $("<td>");
+      newCell1.text(snapshot.val().trainName);
+      newRow.append(newCell1);
+
+      var newCell2 = $("<td>");
+      newCell2.text(snapshot.val().destination)
+      newRow.append(newCell2);
+
+      var newCell3 = $("<td>");
+      newCell3.text(snapshot.val().firstTrainTime);
+      newRow.append(newCell3);
+
+      var newCell4 = $("<td>");
+      newCell4.text(snapshot.val().frequency);
+      newRow.append(newCell4);
+
+      // var newCell5 = $("<td>");
+      // newCell5.text(moment().format(firstTrainTime + frequency));
+      // newRow.append(newCell5);
+      
+      $("#trainSchedule").children().eq(0).append(newRow);
+
+      }, function(errorObject) {
+        console.log("Failed: " + errorObject.code);
+      });
+
+    }
+  }
 
   $("#submit").on("click", function(){
     event.preventDefault();
-    console.log("All Aboard!")
     trainInfo.addRow();
   });
 
