ed138051c11df68dd6e11ff39da374d46b8d0894
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-01-21 17:51:08 -0500

    Got the table data to show up, but now I need to perfect moment.js. I have moment.js situated, but need to get the times correctly calculated.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-// moment().format();
-
 $(document).ready(function() {
 
   // Initialize Firebase
@@ -21,63 +19,75 @@ $(document).ready(function() {
     firstTrainTime: "",
     frequency: "",
     nextArrival: "",
-    minutesAway: "",
-    addRow: function(){
+    minutesAway: ""
+  }
 
-      trainInfo.trainName = $("#trainName").val().trim();
-      trainInfo.destination = $("#destination").val().trim();
-      trainInfo.firstTrainTime = $("#time").val().trim();
-      trainInfo.frequency = parseInt($("#frequency").val().trim());
+  database.ref().on("child_added", function(childSnapshot){
+        
+        console.log(childSnapshot.val().trainName);
+        console.log(childSnapshot.val().destination);
+        console.log(childSnapshot.val().frequency);
+        console.log(childSnapshot.val().firstTrainTime);
 
-      database.ref().set({
+        firstTrainTime = (childSnapshot.val().firstTrainTime);
+
+        var arrivalTime = moment(firstTrainTime, "H HH").format("X");
+        var currentTime = moment();
+
+        var timeDifference = moment().diff(moment(arrivalTime), "minutes");
+        var timeRemaining = timeDifference % frequency;
+
+        trainInfo.minutesAway = frequency - timeRemaining;
+        trainInfo.nextArrival = moment().add(trainInfo.minutesAway, "minutes");
+
+        var newRow = $("<tr class='newRow'>");
+
+        var newCell1 = $("<td>");
+        newCell1.text(childSnapshot.val().trainName);
+        newRow.append(newCell1);
+
+        var newCell2 = $("<td>");
+        newCell2.text(childSnapshot.val().destination)
+        newRow.append(newCell2);
+
+        var newCell3 = $("<td>");
+        newCell3.text(childSnapshot.val().frequency);
+        newRow.append(newCell3);
+
+        var newCell4 = $("<td>");
+        newCell4.text(trainInfo.nextArrival);
+        newRow.append(newCell4);
+
+        var newCell5 = $("<td>");
+        newCell5.text(trainInfo.minutesAway);
+        newRow.append(newCell5);
+      
+        $("#trainSchedule").children().eq(0).append(newRow);
+
+      }, function(errorObject) {
+        console.log("Failed: " + errorObject.code);
+  });
+
+  $("#submit").on("click", function(){
+    event.preventDefault();
+    
+    trainInfo.trainName = $("#trainName").val().trim();
+    trainInfo.destination = $("#destination").val().trim();
+    trainInfo.firstTrainTime = $("#time").val().trim();
+    trainInfo.frequency = parseInt($("#frequency").val().trim());
+
+    database.ref().push({
       trainName: trainInfo.trainName,
       destination: trainInfo.destination,
       frequency: trainInfo.frequency,
       firstTrainTime: trainInfo.firstTrainTime,
-      // nextArrival: moment().format(trainInfo.firstTrainTime + trainInfo.frequency),
-      // minutesAway: moment().format(current time - (trainInfo.firstTrainTime + trainInfo.frequency))
-      });
+    });
+    
+    $("#trainName").val("");
+    $("#frequency").val("");
+    $("#destination").val("");
+    $("#time").val("");
 
-      database.ref().on("value", function(snapshot){
-        console.log(snapshot.val().trainName);
-        console.log(snapshot.val().destination);
-        console.log(snapshot.val().frequency);
-        console.log(snapshot.val().firstTrainTime);
-
-      var newRow = $("<tr class='newRow'>");
-
-      var newCell1 = $("<td>");
-      newCell1.text(snapshot.val().trainName);
-      newRow.append(newCell1);
-
-      var newCell2 = $("<td>");
-      newCell2.text(snapshot.val().destination)
-      newRow.append(newCell2);
-
-      var newCell3 = $("<td>");
-      newCell3.text(snapshot.val().firstTrainTime);
-      newRow.append(newCell3);
-
-      var newCell4 = $("<td>");
-      newCell4.text(snapshot.val().frequency);
-      newRow.append(newCell4);
-
-      // var newCell5 = $("<td>");
-      // newCell5.text(moment().format(firstTrainTime + frequency));
-      // newRow.append(newCell5);
-      
-      $("#trainSchedule").children().eq(0).append(newRow);
-
-      }, function(errorObject) {
-        console.log("Failed: " + errorObject.code);
-      });
-
-    }
-  }
-
-  $("#submit").on("click", function(){
-    event.preventDefault();
-    trainInfo.addRow();
   });
 
 });
